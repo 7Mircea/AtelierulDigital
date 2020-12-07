7bbae00c6c8cf2577a26de601e1612cd934072e5
Gheoace Mircea <7mircea.c@gmail.com>
2020-12-07 12:12:52 +0200

    first commit

--- app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
+++ app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.myfundamentalsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editTextEmail = findViewById(R.id.email);
+        final EditText editTextPhone = findViewById(R.id.phone);
+        Button buttonSubmit = findViewById(R.id.submit);
+        final CheckBox checkBox = findViewById(R.id.checkbox);
+        final TextView textViewAfis = findViewById(R.id.textViewAfis);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                String email = editTextEmail.getText().toString();
+                String phone = editTextPhone.getText().toString();
+                boolean t_c = checkBox.isChecked();
+                boolean validEmail = email.trim().matches(emailPattern);
+                if (email.isEmpty() || phone.isEmpty() || !t_c){
+                    Toast.makeText(MainActivity.this,R.string.notifSecondChallenge,Toast.LENGTH_SHORT).show();
+                }else if(validEmail) {
+                    textViewAfis.setText(getString(R.string.afisSecondChallenge, email, phone, t_c));
+                } else if(!validEmail) {
+                    Toast.makeText(MainActivity.this,R.string.invalidEmail,Toast.LENGTH_SHORT).show();
+                } else if (phone.length() != 10) {
+                    Toast.makeText(MainActivity.this,R.string.invalidEmail,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+
 }
