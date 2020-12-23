ced32005f292c36c8476b584d0aac8ba537031ff
Gheoace Mircea <7mircea.c@gmail.com>
2020-12-23 10:40:15 +0200

    some attempts;

--- app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
+++ app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myfundamentalsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.main_fragment);
+        //Fragment fragment = getFragmentManager().findFragmentById(R.id.main_fragment);
+        Bundle bundle = new Bundle();
+        bundle.putString("cheie","Am trimis un Bundle");
+        if (fragment != null)
+            fragment.setArguments(bundle);
+        else
+            Toast.makeText(MainActivity.this,"There's no fragment",Toast.LENGTH_LONG).show();
+
     }
 }
