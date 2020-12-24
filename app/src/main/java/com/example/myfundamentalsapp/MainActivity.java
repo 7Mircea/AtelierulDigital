88d10559432c43db8c5862785883e0d87fc57fb9
Gheoace Mircea <7mircea.c@gmail.com>
2020-12-24 13:51:39 +0200

    partial implementation of nr 3 of fifth challenge actually(nr 1 and 2 were to read some slides)

--- app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
+++ app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.myfundamentalsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Fragment;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment fragment = getFragmentManager().findFragmentById(R.id.main_fragment);
-        //Fragment fragment = getFragmentManager().findFragmentById(R.id.main_fragment);
+        TextView textView = findViewById(R.id.text_view_activity);
+        Button button = findViewById(R.id.button_send_to_fragment);
+        final MainFragment mainFragment = new MainFragment();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBundle(mainFragment);
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_fragment,mainFragment,"tag").commit();
+
+    }
+
+    private void sendBundle(MainFragment mainFragment) {
         Bundle bundle = new Bundle();
         bundle.putString("cheie","Am trimis un Bundle");
-        if (fragment != null)
-            fragment.setArguments(bundle);
-        else
-            Toast.makeText(MainActivity.this,"There's no fragment",Toast.LENGTH_LONG).show();
-
+        mainFragment.setArguments(bundle);
     }
 }
--- app/src/main/java/com/example/myfundamentalsapp/MainFragment.java
+++ app/src/main/java/com/example/myfundamentalsapp/MainFragment.java
@@ -1,9 +1,14 @@
 package com.example.myfundamentalsapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,6 +29,33 @@ public class MainFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final Activity activity = getActivity();
+        Button button_send = null;
+        Button button_check = null;
+        final TextView textView = view.findViewById(R.id.text_view_fragment);
+
+        if (activity != null) {
+            button_check = activity.findViewById(R.id.button_check_fragment);
+            button_check.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Bundle bundle = getArguments();
+                    if(bundle != null) {
+                        String val = bundle.getString("cheie");
+                        textView.setText(val);
+                    } else {
+                        Toast.makeText(getActivity(),"Fragmentul nu a primit nici un bundle"
+                                ,Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+            button_send = activity.findViewById(R.id.button_send_to_activity);
+
+        }
+
+
+
     }
 
     @Override
