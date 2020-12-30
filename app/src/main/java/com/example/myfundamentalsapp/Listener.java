41b3e8b35bb4c0a59fec2a2f5afecb3dac16f78c
Gheoace Mircea <7mircea.c@gmail.com>
2020-12-30 12:54:10 +0200

    add nav_header.xml, remove some unwanted code

--- app/src/main/java/com/example/myfundamentalsapp/Listener.java
+++
@@ -1,5 +0,0 @@
-package com.example.myfundamentalsapp;
-
-public interface Listener {
-    public void add (int a, int b);
-}
--- app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
+++ app/src/main/java/com/example/myfundamentalsapp/MainActivity.java
@@ -1,31 +1,38 @@
 package com.example.myfundamentalsapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements Listener {
+public class MainActivity extends AppCompatActivity  {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MainFragment mainFragment = new MainFragment();
-        SecondFragment secondFragment = new SecondFragment();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_fragment,mainFragment,"tag").commit();
-        getSupportFragmentManager().beginTransaction().add(R.id.second_fragment,secondFragment,"tag2")
-        .commit();
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar
+                ,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
-    public void add(int a, int b) {
-        SecondFragment fragment = (SecondFragment) getSupportFragmentManager().findFragmentByTag("tag2");
-        fragment.addTwoNumbers(a,b);
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
